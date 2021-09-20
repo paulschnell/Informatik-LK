17062b20d4525e7595bf58d9c3773f195d1d2b91
erwartet <64025517+erwartet@users.noreply.github.com>
2021-09-20 18:32:16 +0200

    k

--- Hundepension/src/de/paulschnell/hundepension/Hundepension.java
+++ Hundepension/src/de/paulschnell/hundepension/Hundepension.java
@@ -5,32 +5,32 @@ import javax.swing.JOptionPane;
 public class Hundepension {
 	
 	public static void main(String[] args) {
-		String name = JOptionPane.showInputDialog("Gebe einen Namen für einen Hund ein:");
+		String name = JOptionPane.showInputDialog("Gebe einen Namen fÃ¼r einen Hund ein:");
 		Double gewicht = 8000.0;
 
-		String eingabe = JOptionPane.showInputDialog("Gebe 1 ein, um " + name + " zu füttern (+200g).\n"
+		String eingabe = JOptionPane.showInputDialog("Gebe 1 ein, um " + name + " zu fÃ¼ttern (+200g).\n"
 				+ "Gebe 2 ein, um mit " + name + " Gassi zu gehen (-100g).");
 		
 		while (gewicht > 4000 && gewicht < 30000 && !eingabe.equals("0")) {
 			if (eingabe.equals("1")) {
 				gewicht += 200.0;
-				eingabe = JOptionPane.showInputDialog(null, "Du hast " + name + " gefüttert. Aktuelles Gewicht: " + gewicht + "g.\n"
-						+ "Gebe 1 ein, um " + name + " zu füttern (+200g).\n"
+				eingabe = JOptionPane.showInputDialog(null, "Du hast " + name + " gefÃ¼ttert. Aktuelles Gewicht: " + gewicht + "g.\n"
+						+ "Gebe 1 ein, um " + name + " zu fÃ¼ttern (+200g).\n"
 						+ "Gebe 2 ein, um mit " + name + " Gassi zu gehen (-100g).\n"
-						+ "Gebe 0 zum Schließen des Programms ein.");
+						+ "Gebe 0 zum SchlieÃŸen des Programms ein.");
 				
 			} else if (eingabe.equals("2")) {
 				gewicht -= 100.0;
 				eingabe = JOptionPane.showInputDialog(null, "Du bist mit " + name + " Gassi gelaufen. Aktuelles Gewicht: " + gewicht + "g.\n"
-						+ "Gebe 1 ein, um " + name + " zu füttern (+200g).\n"
+						+ "Gebe 1 ein, um " + name + " zu fÃ¼ttern (+200g).\n"
 						+ "Gebe 2 ein, um mit " + name + " Gassi zu gehen (-100g).\n"
-						+ "Gebe 0 zum Schließen des Programms ein.");				
+						+ "Gebe 0 zum SchlieÃŸen des Programms ein.");				
 			
 			} else {
 				eingabe = JOptionPane.showInputDialog("Bitter versuche es erneut.\n"
-						+ "Gebe 1 ein, um " + name + " zu füttern (+200g).\n"
+						+ "Gebe 1 ein, um " + name + " zu fÃ¼ttern (+200g).\n"
 						+ "Gebe 2 ein, um mit " + name + " Gassi zu gehen (-100g).\n"
-						+ "Gebe 0 zum Schließen des Programms ein.");
+						+ "Gebe 0 zum SchlieÃŸen des Programms ein.");
 			}
 		}
 		
@@ -38,8 +38,8 @@ public class Hundepension {
 			JOptionPane.showMessageDialog(null, "Du hast die Hundepension abgebrochen.\n"
 					+ "Letztes Gewicht von " + name + ": " + gewicht + "g");
 		} else {
-			JOptionPane.showMessageDialog(null, name + " ist gestorben. Du hast dich nicht genug um ihn gekümmert.\n"
-					+ "Letztes Gewicht: " + gewicht + "k");
+			JOptionPane.showMessageDialog(null, name + " ist gestorben. Du hast dich nicht genug um ihn gekÃ¼mmert.\n"
+					+ "Letztes Gewicht: " + gewicht + "g");
 		}
 	}
 	
