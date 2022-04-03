8938ee7c2eb063fcb5e5dd5e59a431f432c2cd09
alskotim <64025517+alskotim@users.noreply.github.com>
2022-04-03 21:57:35 +0200

    Update Binomi.java

--- Binomi/src/de/paulschnell/binomi/Binomi.java
+++ Binomi/src/de/paulschnell/binomi/Binomi.java
@@ -13,7 +13,16 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Event;
+
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+import java.awt.event.AdjustmentListener;
+import java.awt.event.AdjustmentEvent;
 
 public class Binomi extends JFrame {
 
@@ -23,6 +32,7 @@ public class Binomi extends JFrame {
 	private JTextField tfBiKoeff_k;
 	private JLabel lblBiKoeffError;
 	private JTextField tfBiForm_n;
+	private JTextField tfPasDrei_n;
 
 	/**
 	 * Launch the application.
@@ -44,6 +54,22 @@ public class Binomi extends JFrame {
 	 * Create the frame.
 	 */
 	public Binomi() {
+		setTitle("Binomi");
+		String[] exponenten = new String[12 + 1]; // ⁰¹²³⁴⁵⁶⁷⁸⁹
+		exponenten[0] = "⁰";
+		exponenten[1] = "¹";
+		exponenten[2] = "²";
+		exponenten[3] = "³";
+		exponenten[4] = "⁴";
+		exponenten[5] = "⁵";
+		exponenten[6] = "⁶";
+		exponenten[7] = "⁷";
+		exponenten[8] = "⁸";
+		exponenten[9] = "⁹";
+		exponenten[10] = "¹⁰";
+		exponenten[11] = "¹¹";
+		exponenten[12] = "¹²";
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1280, 720);
 		contentPane = new JPanel();
@@ -108,56 +134,151 @@ public class Binomi extends JFrame {
 		JLabel lblErikWarHier = new JLabel("Erik war hier ඞ");
 		lblErikWarHier.setBounds(1167, 670, 89, 13);
 		contentPane.add(lblErikWarHier);
-		
+
 		lblBiKoeffError = new JLabel("");
 		lblBiKoeffError.setForeground(Color.RED);
 		lblBiKoeffError.setBounds(10, 114, 279, 23);
 		contentPane.add(lblBiKoeffError);
-		
+
 		JLabel lblBiForm = new JLabel("Koeffizienten Binomische Formel");
 		lblBiForm.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblBiForm.setBounds(10, 165, 237, 21);
 		contentPane.add(lblBiForm);
-		
+
 		JLabel lblBiFormFormel = new JLabel("");
 		lblBiFormFormel.setBackground(Color.WHITE);
 		lblBiFormFormel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblBiFormFormel.setBounds(10, 204, 291, 42);
+		lblBiFormFormel.setBounds(10, 237, 1244, 27);
 		contentPane.add(lblBiFormFormel);
-		
+
+		JLabel lblBiFormKoeffizienten = new JLabel("");
+		lblBiFormKoeffizienten.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblBiFormKoeffizienten.setBackground(Color.WHITE);
+		lblBiFormKoeffizienten.setBounds(10, 263, 1244, 27);
+		contentPane.add(lblBiFormKoeffizienten);
+
 		tfBiForm_n = new JTextField();
 		tfBiForm_n.setText("0");
 		tfBiForm_n.setHorizontalAlignment(SwingConstants.CENTER);
 		tfBiForm_n.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		tfBiForm_n.setColumns(10);
-		tfBiForm_n.setBounds(46, 253, 51, 23);
+		tfBiForm_n.setBounds(46, 203, 51, 23);
 		contentPane.add(tfBiForm_n);
-		
+
 		JLabel lblBiForm_n = new JLabel("n:");
 		lblBiForm_n.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblBiForm_n.setBounds(10, 254, 25, 20);
+		lblBiForm_n.setBounds(10, 204, 25, 20);
 		contentPane.add(lblBiForm_n);
-		
+
 		JButton btnBiFormAusrechnen = new JButton("=");
 		btnBiFormAusrechnen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int n = Integer.parseInt(tfBiForm_n.getText());
-				int[] koeffizienten = new int[n];
-				koeffizienten[0] = koeffizienten[n - 1] = 1;
-				
-				for (int k = 0; k < n; k++) {
-					if (n % 2 == 0)
-					koeffizienten[k] = k < (n / 2) ? binKoeff(n, k) : koeffizienten[];
+				int[] koeffizienten = new int[n + 1];
+
+				for (int k = 0; k <= n; k++)
+					koeffizienten[k] = binKoeff(n, k);
+
+				if (n < exponenten.length) {
+
+					lblBiFormFormel.setText(koeffizienten[0] + "a" + exponenten[n]);
+					lblBiFormKoeffizienten.setText("" + koeffizienten[0]);
+					for (int k = 1; k < n; k++) {
+						lblBiFormFormel.setText(lblBiFormFormel.getText() + " + " + koeffizienten[k] + "a"
+								+ exponenten[n - k] + "b" + exponenten[k]);
+
+						lblBiFormKoeffizienten.setText(lblBiFormKoeffizienten.getText() + ", " + koeffizienten[k]);
+					}
+					lblBiFormFormel.setText(lblBiFormFormel.getText() + " + " + koeffizienten[n] + "b" + exponenten[n]);
+					lblBiFormKoeffizienten.setText(lblBiFormKoeffizienten.getText() + ", " + koeffizienten[n]);
+
+				} else {
+
+					lblBiFormFormel.setText(koeffizienten[0] + "a^" + n);
+					lblBiFormKoeffizienten.setText("" + koeffizienten[0]);
+					for (int k = 1; k < n; k++) {
+						lblBiFormFormel.setText(
+								lblBiFormFormel.getText() + " + " + koeffizienten[k] + "a^" + (n - k) + " × b^" + k);
+
+						lblBiFormKoeffizienten.setText(lblBiFormKoeffizienten.getText() + ", " + koeffizienten[k]);
+					}
+					lblBiFormFormel.setText(lblBiFormFormel.getText() + " + " + koeffizienten[n] + "b^" + n);
+					lblBiFormKoeffizienten.setText(lblBiFormKoeffizienten.getText() + ", " + koeffizienten[n]);
+
 				}
-				
-				lblBiFormFormel.setText("");
-				for (int k = 0; k < n; k++)
-					lblBiFormFormel.setText(lblBiFormFormel.getText() + " " + koeffizienten[k]);
 			}
 		});
 		btnBiFormAusrechnen.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		btnBiFormAusrechnen.setBounds(110, 247, 61, 35);
+		btnBiFormAusrechnen.setBounds(110, 197, 61, 35);
 		contentPane.add(btnBiFormAusrechnen);
+
+		JLabel lblPasDrei = new JLabel("Pascal'sche Dreieck bis n");
+		lblPasDrei.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPasDrei.setBounds(10, 324, 174, 21);
+		contentPane.add(lblPasDrei);
+
+		JLabel lblPasDrei_n = new JLabel("n:");
+		lblPasDrei_n.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPasDrei_n.setBounds(10, 356, 25, 20);
+		contentPane.add(lblPasDrei_n);
+
+		tfPasDrei_n = new JTextField();
+		tfPasDrei_n.setText("0");
+		tfPasDrei_n.setHorizontalAlignment(SwingConstants.CENTER);
+		tfPasDrei_n.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tfPasDrei_n.setColumns(10);
+		tfPasDrei_n.setBounds(46, 356, 51, 23);
+		contentPane.add(tfPasDrei_n);
+
+		JTextArea taPasDreiDreieck = new JTextArea();
+		taPasDreiDreieck.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		taPasDreiDreieck.setBounds(10, 387, 1219, 272);
+		taPasDreiDreieck.setEditable(false);
+		taPasDreiDreieck.setText("1");
+		contentPane.add(taPasDreiDreieck);
+
+		JScrollPane scrollBar = new JScrollPane(taPasDreiDreieck);
+		scrollBar.setBounds(10, 387, 1219, 272);
+		contentPane.add(scrollBar);
+
+		JButton btnPasDreiAusrechnen = new JButton("=");
+		btnPasDreiAusrechnen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				taPasDreiDreieckAusrechnen(taPasDreiDreieck);
+
+			}
+		});
+		btnPasDreiAusrechnen.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnPasDreiAusrechnen.setBounds(110, 349, 61, 35);
+		contentPane.add(btnPasDreiAusrechnen);
+
+		JButton btnPasDrei_n_a = new JButton("+");
+		btnPasDrei_n_a.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				tfPasDrei_n.setText("" + (Integer.parseInt(tfPasDrei_n.getText()) + 1));
+				taPasDreiDreieckAusrechnen(taPasDreiDreieck);
+
+			}
+		});
+		btnPasDrei_n_a.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnPasDrei_n_a.setBounds(181, 349, 61, 35);
+		contentPane.add(btnPasDrei_n_a);
+
+		JButton btnPasDrei_n_s = new JButton("-");
+		btnPasDrei_n_s.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				if (Integer.parseInt(tfPasDrei_n.getText()) > 0)
+					tfPasDrei_n.setText("" + (Integer.parseInt(tfPasDrei_n.getText()) - 1));
+				taPasDreiDreieckAusrechnen(taPasDreiDreieck);
+
+			}
+		});
+		btnPasDrei_n_s.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnPasDrei_n_s.setBounds(254, 349, 61, 35);
+		contentPane.add(btnPasDrei_n_s);
 	}
 
 	public int binKoeff(int n, int k) {
@@ -165,5 +286,28 @@ public class Binomi extends JFrame {
 			return 1;
 		else
 			return binKoeff(n - 1, k) + binKoeff(n - 1, k - 1);
+	}
+
+	public void taPasDreiDreieckAusrechnen(JTextArea taPasDreiDreieck) {
+		int n = Integer.parseInt(tfPasDrei_n.getText());
+		int[][] dreieck = new int[n + 1][1];
+
+		dreieck[0][0] = 1;
+
+		for (int m = n; m > 0; m--) {
+			dreieck[m] = new int[m + 1];
+			for (int k = 0; k < dreieck[m].length; k++)
+				dreieck[m][k] = binKoeff(m, k);
+		}
+
+		taPasDreiDreieck.setText("");
+		for (int m = 0; m <= n; m++) {
+			if (m != 0)
+				taPasDreiDreieck.setText(taPasDreiDreieck.getText() + "\n");
+
+			for (int k = 0; k < dreieck[m].length; k++) {
+				taPasDreiDreieck.setText(taPasDreiDreieck.getText() + dreieck[m][k] + " ");
+			}
+		}
 	}
 }
