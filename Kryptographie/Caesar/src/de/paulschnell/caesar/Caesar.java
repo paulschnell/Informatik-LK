663bc8839da60e655558bdb23df303004a0d34a0
erwartet <paul.schnell06@web.de>
2023-02-24 11:37:57 +0100

    Kryptographie Caesar, Vigenere & Matrix

--- Kryptographie/Caesar/src/de/paulschnell/caesar/Caesar.java
+++ Kryptographie/Caesar/src/de/paulschnell/caesar/Caesar.java
@@ -10,6 +10,8 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import java.awt.ScrollPane;
 
 public class Caesar extends JFrame {
 
@@ -51,6 +53,7 @@ public class Caesar extends JFrame {
 
 		JTextArea taOutput = new JTextArea();
 		taOutput.setBounds(454, 40, 354, 215);
+		taOutput.setLineWrap(true);
 		contentPane.add(taOutput);
 
 		tfKey = new JTextField();
@@ -66,7 +69,7 @@ public class Caesar extends JFrame {
 					return;
 				}
 				
-				int key = (int) tfKey.getText().charAt(0);
+				int key = Integer.parseInt(tfKey.getText());
 				String input = taInput.getText();
 				StringBuilder result = new StringBuilder();
 
@@ -76,12 +79,8 @@ public class Caesar extends JFrame {
 				}
 				
 				for (int i = 0; i < input.length(); i++) {
-					char c = (char)(input.charAt(i) + key);
-					if (c > 127) {
-						result.append((char)(c - 128));
-					} else {
-						result.append(c);
-					}
+					int c = (int)(input.charAt(i)) + key;
+					result.append((char)c);
 				}
 				
 				taOutput.setText(result.toString());
@@ -91,15 +90,54 @@ public class Caesar extends JFrame {
 		contentPane.add(btnVerschluesseln);
 
 		JButton btnEntschluesseln = new JButton("Entschluesseln");
+		btnEntschluesseln.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tfKey.getText().length() == 0) {
+					taOutput.setText("");
+					return;
+				}
+				
+				int key = (int) tfKey.getText().charAt(0);
+				String output = taOutput.getText();
+				StringBuilder result = new StringBuilder();
+
+				if (output.length() == 0) {
+					taInput.setText("");
+					return;
+				}
+				
+				for (int i = 0; i < output.length(); i++) {
+					int c = ((int)(output.charAt(i)) - key);
+					result.append((char)c);
+				}
+				
+				taInput.setText(result.toString());
+			}
+		});
 		btnEntschluesseln.setBounds(454, 361, 115, 42);
 		contentPane.add(btnEntschluesseln);
 
 		JButton btnEnde = new JButton("Ende");
+		btnEnde.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		btnEnde.setBounds(723, 385, 85, 28);
 		contentPane.add(btnEnde);
 
 		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				taInput.setText("");
+				taOutput.setText("");
+				tfKey.setText("");
+			}
+		});
 		btnClear.setBounds(723, 347, 85, 28);
 		contentPane.add(btnClear);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setBounds(454, 40, 129, 215);
+		taOutput.add(scrollPane);
 	}
 }
